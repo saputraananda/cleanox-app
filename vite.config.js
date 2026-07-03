abb7fc01627cda7e4866ca57ccbdc85a9eb99761
saputraananda <saputraananda70@gmail.com>
2026-07-03 14:42:58 +0700

    update : esbuild up version

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022',
+    },
+  },
   server: {
     port: 5173,
     proxy: {
